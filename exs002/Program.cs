fea70cfcfc71932c7d6e8ec300576a2e96e391f2
Belova <sonia333@yandex.ru>
2021-11-27 17:39:05 +0700

    done 002

---
+++ exs002/Program.cs
@@ -0,0 +1,17 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.WriteLine("Введите первое число ");
+string? firstString = Console.ReadLine();
+int first = Convert.ToInt32(firstString);
+Console.WriteLine("Введите второе число");
+string? secondString = Console.ReadLine();
+int second = Convert.ToInt32(secondString);
+int stepen = 2;
+if (first == Math.Pow(second, stepen))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
