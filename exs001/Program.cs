50a7f094f3098f17f767c78564f2273b15eae43c
Belova <sonia333@yandex.ru>
2021-11-27 16:09:12 +0700

    exs001 done

---
+++ exs001/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace kvadrat
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Ведите число: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("{0} в квадрате {1}", a, Math.Pow(a, 2));
+            Console.ReadLine();
+        }
+    }
+}
