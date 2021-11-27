6951faad52c79a7fc8998fe6b246bc72cf78fc92
Belova <sonia333@yandex.ru>
2021-11-27 16:20:14 +0700

    error

--- exs001/Program.cs
+++ exs001/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main()
         {
-            Console.Write("Ведите число: ");
+            Console.Write("Введите число: ");
             double a = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("{0} в квадрате {1}", a, Math.Pow(a, 2));
             Console.ReadLine();
