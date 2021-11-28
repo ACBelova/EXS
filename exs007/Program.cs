d101bd05f5ec5966a106e8856f710cd4143f79b1
Belova <sonia333@yandex.ru>
2021-11-28 12:52:12 +0700

    add exs007

---
+++ exs007/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+Console.WriteLine("Введите число ");
+int a;
+a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("число чётное");
+}
+else
+{
+    Console.WriteLine("число нечётное");
+}
