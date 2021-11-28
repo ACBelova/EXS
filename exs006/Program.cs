784e14230d01aa918ef54f53a32ef39739315c50
Belova <sonia333@yandex.ru>
2021-11-28 12:30:11 +0700

    add exs006

---
+++ exs006/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу вычисления значения функции y = f(x), где f(x)= 3х*2+9sqrх
+
+double f(double x)
+{
+    return 3 * Math.Pow(2, x) + 9 * Math.Sqrt(x);
+}
+int x = 2;
+
+double result = 0;
+while (x <= 10)
+{
+    result = result + f(x);
+    x++;
+}
+
+Console.Write("Результат: ");
+Console.WriteLine(result);
+
