6fc0a1f5bfb68312e4987ee4e943501c3d859b05
Belova <sonia333@yandex.ru>
2021-11-28 11:34:26 +0700

    add exs04

--- exs004/Program.cs
+++ exs004/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// По заданному номеру дня недели вывести его название
+Console.WriteLine("Введите порядковый номер дня недели ");
+int num = int.Parse(Console.ReadLine());
+
+string[] dayofWeek = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+
+int n = dayofWeek.Length;
+Console.WriteLine(dayofWeek[num-1]);
+
