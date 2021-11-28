12849fcf3e9afca0adb0effccfaf62ff6c859ea0
Belova <sonia333@yandex.ru>
2021-11-28 19:50:54 +0700

    add exs008

--- exs008/Program.cs
+++ exs008/Program.cs
@@ -1,2 +1,7 @@
 ﻿// Показать числа от -N до N
-
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = -N; i <=N; i++)
+{
+    Console.Write("{0}, ", (i));
+}
