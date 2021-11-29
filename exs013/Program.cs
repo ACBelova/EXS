2026c33f6c5669f428affe0bb527ce01a476c21c
Belova <sonia333@yandex.ru>
2021-11-29 18:13:12 +0700

    add exs013

---
+++ exs013/Program.cs
@@ -0,0 +1,10 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число ");
+var n = Math.Abs(Convert.ToInt64(Console.ReadLine()));
+        while (n >= 100)
+        {
+            n /= 10;
+        }
+        var r = n % 10;
+        Console.WriteLine("Вторая цифра ");
+        Console.WriteLine(r);
