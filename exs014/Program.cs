d5a04fe837a6c80b610b59032652386e016101af
Belova <sonia333@yandex.ru>
2021-11-29 22:04:18 +0700

    add exs014, but not enoth

---
+++ exs014/Program.cs
@@ -0,0 +1,8 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Введите число ");
+int a;
+a = Convert.ToInt32(Console.ReadLine());
+if (a % 3 == 0)
+Console.WriteLine("Число {0} кратно 3", a);
+   
+else Console.WriteLine("Число {0} не кратно 3", a);
