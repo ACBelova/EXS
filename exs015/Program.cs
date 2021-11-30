aa0c0573bbae2b2637d4128c08463f8cb20c85d9
Belova <sonia333@yandex.ru>
2021-11-30 19:39:28 +0700

    add exs015

---
+++ exs015/Program.cs
@@ -0,0 +1,5 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Введите число ");
+int a;
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a>99 ? a.ToString()[2] : "третьей цифры нет");
