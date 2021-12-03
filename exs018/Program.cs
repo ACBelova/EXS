ba4254ceb92958fe947954f2409ff93af65aa550
Belova <sonia333@yandex.ru>
2021-12-03 13:17:20 +0700

    add exs018

---
+++ exs018/Program.cs
@@ -0,0 +1,16 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+Console.WriteLine("Введите первое число ");
+string? firstString = Console.ReadLine();
+int first = Convert.ToInt32(firstString);
+Console.WriteLine("Введите второе число");
+string? secondString = Console.ReadLine();
+int second = Convert.ToInt32(secondString);
+int stepen = 2;
+if (first == Math.Pow(second, stepen))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
