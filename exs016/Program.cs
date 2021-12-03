b6750c3abfbf819fcff4c760b02fa8139d9f991e
Belova <sonia333@yandex.ru>
2021-12-03 12:28:20 +0700

    add exs016

---
+++ exs016/Program.cs
@@ -0,0 +1,11 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+Console.WriteLine("Введите число ");
+int i;
+Int32.TryParse(Console.ReadLine(), out i);
+Console.Write((i % 7 == 0) && (i % 23 == 0) ? "кратно 7 и 23" : "не кратно 7 и 23");
+Console.ReadLine();
+
+
+
+
+
