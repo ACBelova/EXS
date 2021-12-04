c8c67cf470e97b820b584cf8214651a92b6028f5
Belova <sonia333@yandex.ru>
2021-12-04 10:56:22 +0700

    add exs019

---
+++ exs019/Program.cs
@@ -0,0 +1,13 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+// X  Y  X∨Y ¬X∨Y	¬X	¬Y	¬X∧¬Y  ¬X∨Y≡¬X∧¬Y
+// 0  0	 0  	1	 1   1	  1	     1
+// 0  1  1  	0	 1   0	  0	     1
+// 1  0  1  	0    0	 1	  0	     1
+// 1  1	 1  	0    0	 0	  0	     1
+
+Console.Write("Введите значение x true или false: ");
+bool x = bool.Parse(Console.ReadLine());
+Console.Write("Введите значение y true или false: ");
+bool y = bool.Parse(Console.ReadLine());
+bool a =!(x||y) == !x ^ !y;
+Console.WriteLine(a);
