52f3ba6c4b3279ff11ea04de5c26e4470fb64cdd
Belova <sonia333@yandex.ru>
2021-12-05 18:53:32 +0700

    add exs028

---
+++ exs028/Program.cs
@@ -0,0 +1,10 @@
+﻿// Определить количество цифр в числе
+Console.WriteLine("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int digits = 0; // кол-во цифр
+while (A > 0)
+{
+  digits++;
+  A = (int)(A/10);
+}
+Console.WriteLine($"В этом числе содержится: {digits} цифр ");
