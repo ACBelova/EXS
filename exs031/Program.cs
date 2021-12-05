bb5d8274f9ffcb41a3b6da79dd63eb41190abe87
Belova <sonia333@yandex.ru>
2021-12-05 19:58:41 +0700

    stop to exs031

---
+++ exs031/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+while (x%2==0)
+{
+  for (int i = 1; i <= x; i++)
+{
+    Console.WriteLine($"Куб числа {i} = {Math.Pow(i,3)}");
+
+}
+}
