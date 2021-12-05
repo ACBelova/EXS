71597cd17c0eaa02c4ed192511160974b88596b0
Belova <sonia333@yandex.ru>
2021-12-05 09:30:21 +0700

    add exs021

--- exs019/Program.cs
+++ exs019/Program.cs
@@ -5,9 +5,19 @@
 // 1  0  1  	0    0	 1	  0	     1
 // 1  1	 1  	0    0	 0	  0	     1
 
-Console.Write("Введите значение x true или false: ");
-bool x = bool.Parse(Console.ReadLine());
-Console.Write("Введите значение y true или false: ");
-bool y = bool.Parse(Console.ReadLine());
-bool a =!(x||y) == !x ^ !y;
-Console.WriteLine(a);
+bool [] x= {false, true};
+bool [] y= {false, true };
+int count = 0;
+for (int f = 0; f < x.Length; f++)
+{
+    for (int d = 0; d < y.Length; d++)
+    {
+        Console.WriteLine($"{x[f]}, {y[d]}");
+        if ((!(x[f] || y[d])) == (!x[f] && !y[d])) count++;
+    }
+}
+if (count==4) Console.WriteLine("Утверждение верно");
+else
+{
+    Console.WriteLine("Утверждение ложно");
+}
---
+++ exs021/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+// I четверть   (10+,10+)
+// II четверть  (-10,+10)
+// III четверть (-10,-10)
+// IV четверть  (10+,-10)
+Console.WriteLine("Введите число от 1 до 4: ");
+double x = Convert.ToDouble(Console.ReadLine());
+if (x == 1)
+{
+    Console.Write("(x < 0) , (y > 0)");
+}
+else if (x == 2)
+{
+    Console.Write("(x < 0) , (y > 0)");
+}
+else if (x == 3)
+{
+    Console.Write("(x < 0) , (y < 0)");
+}
+else if (x == 4)
+{
+    Console.Write("(x > 0) , (y < 0)");
+}
+else 
+{
+    Console.WriteLine("Вы ввели не правильное число!");
+}
