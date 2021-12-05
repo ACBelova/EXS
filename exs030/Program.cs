eaf758e8232bc775d9294ca32033d3bace76d579
Belova <sonia333@yandex.ru>
2021-12-05 19:39:43 +0700

    add exs030

---
+++ exs030/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int composition = 1;
+for (int i = 1; i <= x; i++)
+{
+    composition = composition * i;
+    Console.Write($"{i} * ");
+}
+Console.WriteLine($"= " + composition);
