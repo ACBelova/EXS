58ed0dc0b050971dbdf8372364d4c9f91978065d
Belova <sonia333@yandex.ru>
2021-12-05 12:21:54 +0700

    add exs024

---
+++ exs024/Program.cs
@@ -0,0 +1,11 @@
+﻿// ## Почувствуй себя мидлом*
+// Показать таблицу квадратов чисел от 1 до N 
+
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= x; i++)
+{
+    Console.WriteLine(i*i);
+}
+
