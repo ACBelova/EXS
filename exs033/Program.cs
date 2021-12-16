3bfea5937614da0f84611e4300431a72821ddf3f
Belova <sonia333@yandex.ru>
2021-12-16 18:14:51 +0700

    add exs033

---
+++ exs033/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами и вывести их на экран
+int[] arr = { 1, 0, 0, 1, 1, 0, 0, 1 };
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(arr);
