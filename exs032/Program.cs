07f5f762c9114bb0519b65ecadf8fc702d17c5da
Belova <sonia333@yandex.ru>
2021-12-16 18:08:56 +0700

    add exs032

---
+++ exs032/Program.cs
@@ -0,0 +1,14 @@
+﻿// ## Почувствуй себя сеньором*
+// Задать массив из 8 элементов и вывести их на экран 
+int[] arr = { 1, 8, 9, 5, 7, 5, 4, 6 };
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(arr);
